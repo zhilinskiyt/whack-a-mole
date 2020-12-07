fdbb3f19d6b6d49e8089e0512a91a1aa0d5d10e8
TsimurZhylinski <zhilinskiyt@gmail.com>
2020-12-07 17:13:35 +0300

    feat: add css , html ,js markup

---
+++ main.js
@@ -0,0 +1,53 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const startButton = document.querySelector('.start-button')
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+        console.log('ah nah its the same one');
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    console.log(time, hole);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if(!timeUp) peep();  
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    peep()
+    score = 0;
+    setTimeout(() => timeUp = true, 10000)
+}
+
+function bonk(e) {
+    if(!e.isTrusted) return;
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
+startButton.addEventListener('click', startGame);
